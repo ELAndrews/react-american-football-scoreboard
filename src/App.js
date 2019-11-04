c982745e7b8f82877f20d4631ef923946c977210
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-04 14:13:28 +0000

    Add working Timer function with start/stop btn and reset btn

--- src/App.js
+++ src/App.js
@@ -4,14 +4,45 @@ import React, { useState, useEffect } from 'react';
 import "./App.css";
 import BottomRow from "./BottomRow";
 
-function Timer( state, time) {
+  const Timer = () => {
+    const [time, setTimer] = useState(3600);
+    const [isActive, setIsActive] = useState(false);
 
-  const [ timer, setTimer] = React.useState(0);
+    const TimeDisplay = () => {
+      let minutes = Math.floor(time / 60);
+      let seconds = time % 60;
+
+      minutes = minutes.toString().length === 1 ? "0" + Number(minutes) : Number(minutes)
+      seconds = seconds.toString().length === 1 ? "0" + Number(seconds) : Number(seconds)
+
+      useEffect(() => {
+        let interval = null;
+        if (isActive) {
+          interval = setInterval(() => {
+            setTimer(time => time - 1);
+          }, 1000);
+        } else if (!isActive && time !== 0) {
+          clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+      }, [isActive, time]);
+      return `${minutes}:${seconds}`;
+    }
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setTimer(0);
+    setIsActive(false);
+  }
+
 
   return (
-    <div className='timer'>00:00
-      <button className= "timerBtn">{state ? "Stop" : "Start"}</button>
-      <button className = "timerRest" onClick = {() => setTimer(timer = 0)}>Reset</button>
+    <div className='timer'>{TimeDisplay()}
+      <button className= "timerBtn" onClick = {toggle}>{isActive ? "Stop" : "Start"}</button>
+      <button className = "timerRest" onClick = {reset}>Reset</button>
     </div>
   )
 }
