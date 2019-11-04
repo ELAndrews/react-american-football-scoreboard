f63a174bf10d97420c537b1dd09e02b710d640ae
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-04 15:14:57 +0000

    Add aendScore function

--- src/App.js
+++ src/App.js
@@ -8,9 +8,21 @@ import Timer from "./Timer";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-
+  
   const [homeScore, sethomeScore] = React.useState(0);
   const [awayScore, setawayScore] = React.useState(0);
+
+  //Stretch Task - button handlers
+  
+  function amendScore (team, points) {
+    if (team === "home") {
+      sethomeScore(homeScore + points)
+    } else if (team === "away") {
+      setawayScore(awayScore + points)
+    } else {
+      console.log(`Invalid team entry`)
+    }
+  }
 
   return (
     <div className="container">
@@ -34,12 +46,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick = {() => sethomeScore(homeScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick = {() => sethomeScore(homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick = {() => amendScore('home', 7)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick = {() => amendScore('home', 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button onClick = {() => setawayScore(awayScore + 7)}className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick = {() => setawayScore(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick = {() => amendScore('away', 7)}className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick = {() => amendScore('away', 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
